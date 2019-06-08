ce4ce4b320e795eb4368c18b36f119bfaa584de7
Dallas Lillie <drlillie@fullsail.edu>
2019-06-08 12:28:38 -0700

    Add a powershell script for running our unit tests and uploading the results to appveyor

---
+++ run_tests.ps1
@@ -0,0 +1,10 @@
+﻿# script args
+param([string]$project_dir)
+
+# run tests
+# environment variables for the solution dir should be passed in as an arg to this script
+& $project_dir\bin\$env:PLATFORM\$env:CONFIGURATION\pool_allocator_tests.exe --gtest_output=xml:$project_dir\tests\output\$env:PLATFORM\$env:CONFIGURATION\pool_allocator_tests.xml
+
+# upload results to AppVeyor
+$wc = New-Object 'System.Net.WebClient'
+$wc.UploadFile("https://ci.appveyor.com/api/testresults/junit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path $project_dir\tests\output\$env:PLATFORM\$env:CONFIGURATION\pool_allocator_tests.xml))
